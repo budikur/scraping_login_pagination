45e0d0ba2bf3fd9d2f1c1779e61f5779d28cbd9d
budikur <pakbudi.com@gmail.com>
2020-11-28 20:45:50 +0700

    oke

--- run.py
+++ run.py
@@ -1,9 +1,32 @@
 import requests
 import bs4
 
-url = 'http://localhost:5000/'
-content = requests.get(url)
-#print(content.text)
-asd = bs4.BeautifulSoup(content.text, "html.parser")
-#print(asd)
+def login():
+    print('login...')
+    datas = {
+        'username': 'user',
+        'password': 'user12345'
+    }
 
+    res = requests.post('http://localhost:5000/')
+    f = open('./res.html', 'w+')
+    f.write(res.text)
+    f.close()
+
+def get_urls():
+    print('getting urls...')
+
+def get_detail():
+    print('getting detail...')
+
+def create_csv():
+    print('csv generated...')
+
+def run():
+    login()
+    get_urls()
+    get_detail()
+    create_csv()
+
+if __name__ == '__main__':
+    run()
